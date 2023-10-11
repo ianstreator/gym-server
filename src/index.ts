d777745f61a1073c30af7670a60e485b4a679a20
ian streator <ianstreator+github@gmail.com>
2023-10-11 15:28:56 -0500

    express server connected to MongoDB with mongoose, defined middleware

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import router from "./router/index.js";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors({ origin: "http://localhost:5173" }));
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.URI);
+mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.get("/health", (req, res) => {
+  res.send("Healthy");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`app listening on port:${PORT}`);
+});
+
+app.use("/", router());
