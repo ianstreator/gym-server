1a2056af586d154f116220fa2d8dc03fb7d6064c
ian streator <ianstreator+github@gmail.com>
2023-10-12 14:32:43 -0500

    verify route WIP

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -4,5 +4,6 @@ import { login, register } from "../controllers/authentication.js";
 
 export default (router: express.Router) => {
   router.post("/auth/register", register);
+  // router.post("/auth/verify", verify);
   router.post("/auth/login", login);
 };
