dda58cc6cb58450d3b0edc169c981fce8f2c7faa
ian streator <ianstreator+github@gmail.com>
2023-10-12 14:28:35 -0500

    enhanced error handling. verify controller WIP

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -19,15 +19,7 @@ export const login = async (req: express.Request, res: express.Response) => {
     const user = await getUserByEmail(email).select(
       "+authentication.salt +authentication.password +verified +verification_code"
     );
-    if (!user.verified) {
-      console.log(user.verified, user.verification_code);
 
-      return res
-        .status(403)
-        .send(
-          "please verify your email. if you cannot find your verification email, you can request a new one below."
-        );
-    }
     if (!user) {
       return res.sendStatus(400);
     }
@@ -35,7 +27,13 @@ export const login = async (req: express.Request, res: express.Response) => {
     const expectedHash = authentication(user.authentication.salt, password);
 
     if (user.authentication.password !== expectedHash) {
-      return res.sendStatus(403);
+      return res.status(403).send("email or password is incorrect.");
+    } else if (!user.verified) {
+      return res
+        .status(403)
+        .send(
+          "please verify your email. if you cannot find your verification email, you can request a new one below."
+        );
     }
 
     const salt = random();
@@ -61,24 +59,29 @@ export const login = async (req: express.Request, res: express.Response) => {
 
 export const register = async (req: express.Request, res: express.Response) => {
   try {
-    const { email, username, password } = req.body as {
-      email: string;
-      username: string;
-      password: string;
-    };
-    console.log(req.body);
+    const { email, username, password } = req.body;
+
     if (!email || !username || !password) {
-      return res.sendStatus(400);
+      return res
+        .status(400)
+        .json({
+          message: "You need to fill out all fields to register",
+        })
+        .end();
     }
 
     const existingUser = await getUserByEmail(email);
 
     if (existingUser) {
-      return res.sendStatus(400);
+      return res.status(400).json({
+        message: `The email ${email} is already in use`
+      }).end();
     }
+
     const verificationCode = verificationCodeGen();
     const salt = random();
-    const user = await createUser({
+
+    await createUser({
       email,
       username,
       verified: false,
@@ -89,14 +92,57 @@ export const register = async (req: express.Request, res: express.Response) => {
       },
     });
 
-    sendVerificationEmail({
+    const emailResponse = await sendVerificationEmail({
       userEmail: email,
-      verificationCode: verificationCode,
+      verificationCode,
     });
+    console.log(emailResponse)
 
-    return res.status(200).json(user).end();
+    if (emailResponse === true) {
+      return res
+        .status(200)
+        .json({
+          email_sent: emailResponse,
+          message: `We have sent you a verification code to ${email}.`,
+        })
+        .end();
+    } else {
+      return res
+        .status(400)
+        .json({
+          email_sent: emailResponse,
+          message: `We were unable send ${email} a verification code.`,
+        })
+        .end();
+    }
   } catch (error) {
-    console.log(error);
-    return res.sendStatus(400);
+    return res.sendStatus(500);
   }
 };
+
+// export const verify = async (req: express.Request, res: express.Response) => {
+//   try {
+//     const { email, code } = req.body;
+//     console.log(req.body);
+//     if (!email || !code) {
+//       return res.sendStatus(400);
+//     }
+
+//     const existingUser = await getUserByEmail(email);
+
+//     if (existingUser) {
+//       return res.sendStatus(400);
+//     }
+//     const verificationCode = verificationCodeGen();
+
+//     sendVerificationEmail({
+//       userEmail: email,
+//       verificationCode: verificationCode,
+//     });
+
+//     return res.status(200);
+//   } catch (error) {
+//     console.log(error);
+//     return res.sendStatus(400);
+//   }
+// };
