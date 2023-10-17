b7c3692b089b32a5ad0ab1eeabf658fdcd205d62
ian streator <ianstreator+github@gmail.com>
2023-10-17 00:22:35 -0500

    corrected dev client url to vite default port

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 const PROD_CORS_URL = "https://constfitness.vercel.app";
-const DEV_CORS_URL = "http://localhost:4000";
+const DEV_CORS_URL = "http://localhost:5173";
 const ENV = process.env.RENDER;
-export const CLIENT_CORS_URL = ENV ? PROD_CORS_URL : DEV_CORS_URL;
+export const ENV_CORS_URL = ENV ? PROD_CORS_URL : DEV_CORS_URL;
